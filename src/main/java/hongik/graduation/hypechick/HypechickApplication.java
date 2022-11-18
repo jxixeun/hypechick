ba55ad334e16d1d8ddc842a70f3b1baf57f272e8
jxixeun <jxixeun@gmail.com>
2022-11-18 15:12:48 +0900

    Initial commit

---
+++ src/main/java/hongik/graduation/hypechick/HypechickApplication.java
@@ -0,0 +1,13 @@
+package hongik.graduation.hypechick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HypechickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HypechickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hongik/graduation/hypechick/HypechickApplicationTests.java
@@ -0,0 +1,13 @@
+package hongik.graduation.hypechick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HypechickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
